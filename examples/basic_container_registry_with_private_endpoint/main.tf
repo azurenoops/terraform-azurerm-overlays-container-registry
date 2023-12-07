68fa31eecb9d8fae96cd4623358bff192b9f26c0
wtomaz808 <wtomaz808@gmail.com>
2023-12-07 12:07:43 -1000

    updated module to allow depends_on & providers

--- examples/basic_container_registry_with_private_endpoint/main.tf
+++ examples/basic_container_registry_with_private_endpoint/main.tf
@@ -4,14 +4,6 @@
 #---------------------------------------------------------
 # Azure Region Lookup
 #----------------------------------------------------------
-module "mod_azure_region_lookup" {
-  source  = "azurenoops/overlays-azregions-lookup/azurerm"
-  version = "~> 1.0.0"
-
-  azure_region = "eastus"
-}
-
-
 module "acr" {
   depends_on = [
     azurerm_virtual_network.vnet
--- examples/basic_container_registry_with_private_endpoint/resource_dep.tf
+++ examples/basic_container_registry_with_private_endpoint/resource_dep.tf
@@ -4,6 +4,14 @@
 #-------------------------------------
 # VNET Creation - Default is "true"
 #-------------------------------------
+# This module is used to lookup the Azure Region based on the Azure Region Name
+module "mod_azure_region_lookup" {
+  source  = "azurenoops/overlays-azregions-lookup/azurerm"
+  version = "~> 1.0.0"
+
+  azure_region = "usgovvirginia"
+  
+}
 
 resource "azurerm_resource_group" "rg" {
   name     = "anoa-eus-dev-acr-dev-rg"
@@ -23,4 +31,5 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.100.0/24"]
+  private_endpoint_network_policies_enabled = false
 }
